9e982fa928f25a47580792f1326d03a267791d68
YiroA200 <martin.gallegos04@inacapmail.cl>
2025-11-21 22:20:55 -0300

    Primeras vistas admin
    se agrego los primeros modelos en admin, list_display, list_filter, search_field y el manejo de imagenes, es basico por ahora y aun no da imagen, eso lo vere mas adelante

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,114 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Product, categoryProduct, insumos, pedidos
 
-# Register your models here.
+# admin de productos
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nombre',
+        'category',
+        'precio_base',
+        'display_thumbnail',
+    )
+    
+
+    list_filter = (
+        'category',
+    )
+    
+    search_fields = (
+        'nombre',
+        'description',
+    )
+    
+
+    def display_thumbnail(self, obj):
+        """Muestra una miniatura de la primera imagen del producto."""
+
+        if obj.imagen1:
+
+            return format_html('<img src="{}" style="height: 50px; width: auto; border-radius: 4px;" />', obj.imagen1.url)
+        return "No Image"
+    
+
+    display_thumbnail.short_description = 'Imagen'
+
+ 
+#admin de pedidos
+
+
+@admin.register(pedidos)
+class PedidosAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nombre_cliente',
+        'fecha_creacion',
+        'fecha_entrega',
+        'plataforma',
+        'estados', 
+        'estado_pago',
+        'token_Trakeo',
+    )
+    
+
+    list_filter = (
+        'estados',       
+        'estado_pago',  
+        'plataforma',    
+        'fecha_creacion',
+    )
+    
+
+    search_fields = (
+        'nombre_cliente',
+        'contacto',
+        'descripcion',
+        'token_Trakeo',
+    )
+    
+
+    readonly_fields = ('fecha_creacion', 'token_Trakeo')
+
+
+#admin de insumos
+
+
+@admin.register(insumos)
+class InsumosAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'tipo',
+        'cantidad_disponible',
+        'unidad',
+        'marca',
+        'color',
+    )
+    
+
+    list_filter = (
+        'tipo',
+        'marca',
+        'color',
+    )
+    
+
+    search_fields = (
+        'name',
+        'tipo',
+        'marca',
+    )
+
+
+#categoria
+
+
+@admin.register(categoryProduct)
+class CategoryProductAdmin(admin.ModelAdmin):
+
+    search_fields = ('nombre',)
+    list_display = ('nombre',)
